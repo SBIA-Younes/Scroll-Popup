44df157e823e7df9661c23b4da51f3f8496ae6e1
SBIA-Younes <brahim-sb@hotmail.fr>
2023-11-14 11:07:28 +0100

    add a code for show a picture when we scroll

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
 // Créer un événement au scroll
+let playOnce = true;
+window.addEventListener('scroll', (e)=> {
+  // Navbar effect
+  if (window.scrollY > 50) {
+    navbar.style.height = '45px';
+  } else {
+    
+    navbar.style.height = '90px';
+  }
+
+  // Cree une variable pour obtemir  pourcentage du body
+  let scrollValue = (window.scrollY + window.innerHeight) / document.body.offsetHeight;
+  // Image
+  if (scrollValue > 0.45) {
+    imgImprovise.style.opacity = 1;
+    imgImprovise.style.transform = 'none';
+  }
+
+  // Popup
+  if (scrollValue > 0.85  && playOnce) {
+    popup.style.opacity = 1;
+    popup.style.transform = 'none';
+  }
+
+  closeBtn.addEventListener('click', ()=> {
+    popup.style.opacity = 0;
+    popup.style.transform = 'translateY(100%)'
+    playOnce = false;
+  })
+});
 
 // Réduire la navbar quand on descend vers le vite, la remettre à sa taille initiale si on remonte tout en haut
 
